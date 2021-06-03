d573ba67d47fcb401a183792cdc7797adcb5bcb1
suleymanakilli <suleymanakilli1@gmail.com>
2021-06-03 22:37:35 +0300

    HRMS First Commit

--- src/main/java/hrms/hrms/business/adapters/CloudinaryImageServiceAdapter.java
+++ src/main/java/hrms/hrms/business/adapters/CloudinaryImageServiceAdapter.java
@@ -5,22 +5,33 @@ import com.cloudinary.utils.ObjectUtils;
 import hrms.hrms.core.utilities.results.Result;
 import hrms.hrms.core.utilities.results.SuccessResult;
 import hrms.hrms.entities.concretes.Candidate;
+import lombok.Data;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.Map;
 
+@Data
 @Service
 public class CloudinaryImageServiceAdapter implements CandidateUploadImageService {
     private Cloudinary cloudinary;
+
+    private String url;
+
     @Autowired
-    public CloudinaryImageServiceAdapter() {
-        this.cloudinary=new Cloudinary("cloudinary://343745471297952:dU21SZt_WM2Y0lrfZ7aMc1igovY@dw2jquesf");
+    private Environment env;
+
+    public CloudinaryImageServiceAdapter(Environment env) {
+        this.env=env;
+        this.url="cloudinary://"+env.getProperty("cloudinary.api_key")+":"+env.getProperty("cloudinary.api_secret")+"@"+env.getProperty("cloudinary.cloud_name");
+
+        this.cloudinary=new Cloudinary(this.url);
     }
-
-
 
     @Override
     public Map uploadImage(MultipartFile file) throws IOException {
